700f6de993b87e7ac162c017035d661f087dbaad
Zheondre <zcalcano@microsoft.com>
2023-07-14 07:26:18 -0400

    weekly cp

--- filecrawler/filesearch.cpp
+++ filecrawler/filesearch.cpp
@@ -9,6 +9,21 @@
 
 using namespace std;
 
+// compile with g++ ./filesearch
+
+enum{ 
+    FILECRAWLER, 
+    ADDFILE, 
+    RMFILE, 
+    ADDIR, 
+    RMDIR, 
+    LOAD, 
+    SAVE, 
+    PURGE, 
+    OPTIONS,
+    EXIT, 
+};
+
 enum ntype{ 
     DRV,
     FOLDER, 
@@ -21,6 +36,12 @@ struct nfile{
     string name;
     vector<nfile*> children; 
     nfile(){};
+    ~nfile(){
+        cout << "Removing " << this->name << endl; 
+        this->parent = NULL; 
+        this->name.clear(); 
+        this->children.clear();
+    }
     nfile(string name){ 
         node_type = ntype::FLE; 
         this->name = name;
@@ -40,17 +61,47 @@ struct nfolder : nfile {
 
 };
 
-deque<nfile*> fileseach(nfolder *drive, string filename){
+class flemgr{ 
+
+    string newcontent; 
+    // key: full path to file or folder. Val: file pointer.
+    unordered_map<string, nfile*> filePath; 
+    unordered_map<nfile*, string> filename; 
+    int filecount;
+    //vector<nfile*> drive;
+
+    public:
+    
+    nfolder drive; 
+
+    int mode; 
+    flemgr(){
+        mode = 0;
+        filecount =0;
+    };
+    flemgr(int mode){ 
+        this->mode = mode; 
+        filecount =0;
+    };
+
+    deque<nfile*> fileseach(const string& filename);
+    bool addContent(const string& filename);
+    void setcontentname(string name);
+
+   void buildDirectoryStructure(ifstream&, nfolder*, string);
+};
+
+deque<nfile*> flemgr::fileseach(const string& filename){
 
     unordered_map<nfile*, string> visitedNodes;
     deque<nfile*> filepath; 
     queue<nfile*> bfs; 
 
-    if(drive->children.size() == 0){ 
+    if(drive.children.size() == 0){ 
         return filepath; 
     }
 
-    bfs.push(drive); 
+    bfs.push(&drive); 
 
     while(!bfs.empty()) { 
 
@@ -68,20 +119,47 @@ deque<nfile*> fileseach(nfolder *drive, string filename){
                 bfs.push(currentNode->children[i]);
             }
         } else { 
-            if(currentNode->name != filename){ 
-                continue;
-            }
-            cout << endl<< "Printing File Path.\n" ;
-            while(!bfs.empty()){ 
-                bfs.pop();
-            }
 
             nfile *temp = currentNode;
 
-            while(temp && (temp->node_type != ntype::DRV)){ 
-                filepath.push_back(temp);             
-                cout << temp->name << endl;
-                temp = temp->parent;
+            switch(mode){
+                case FILECRAWLER:
+                    
+                    if(currentNode->name != filename){ 
+                        continue;
+                    }
+                    cout << endl<< "Printing File Path.\n" ;
+                    while(!bfs.empty()){ 
+                        bfs.pop();
+                    }
+
+                    while(temp && (temp->node_type != ntype::DRV)){ 
+                        filepath.push_back(temp);             
+                        cout << temp->name << endl;
+                        temp = temp->parent;
+                    }
+                break;
+                case ADDFILE: 
+                    nfolder *fr = new nfolder(this->newcontent); 
+                    fr->parent = temp;
+                break;
+                case RMFILE: 
+                    cout << "Removing " << temp->name << endl; 
+                    delete(temp);
+                    temp = NULL;
+                break;
+                case ADDIR: 
+                    //addir(filename, &drive);
+                break; 
+                
+                case RMDIR: 
+                break; 
+
+                case PURGE: 
+                    delete(temp); 
+                    temp = NULL;
+                    //filecount--;
+                break;
             }
         }
     }
@@ -90,7 +168,7 @@ deque<nfile*> fileseach(nfolder *drive, string filename){
 }
 
 //#define DEBUG_FILELOAD
-void buildDirectoryStructure(ifstream& file, nfolder* currentFolder){ 
+void flemgr::buildDirectoryStructure(ifstream& file, nfolder* currentFolder, string pwd){ 
     string line; 
     //cout << endl << "In BuildS" <<endl;
 
@@ -128,13 +206,29 @@ void buildDirectoryStructure(ifstream& file, nfolder* currentFolder){
 #ifdef DEBUG_FILELOAD
             cout << currentFolder->name << " added " << fr->name << endl;
 #endif
-            buildDirectoryStructure(file, fr);
+            buildDirectoryStructure(file, fr,  pwd += " -> " + currentFolder->name );
         }
     }
 
 }
 
+//void PathFinder
+void report(){ 
+
+}
+
 int main(int argc, char *argv[]) {
+
+    const char *options =  R""""(
+        0 - Find shortest path between two files.\n 
+        1 - Add file.\n
+        2 - Remove file.\n
+        3 - Add dir.\n
+        4 - Remove dir.\n
+        5 - Load drive structure.\n
+        6 - Save drive structure to file.\n
+        7 - Options.\n
+        8 - Exit.\n)"""";
 
     std::ifstream inputFile("testcase2.txt");
     if(!inputFile){ 
@@ -157,18 +251,78 @@ int main(int argc, char *argv[]) {
     std::cout << "Enter the target file name: ";
     std::cin >> targetFileName;
     */ 
-    nfolder drive; 
+    
+    //nfolder drive; 
 
-    buildDirectoryStructure(inputFile, &drive);
+    int mode = 0;
 
     deque<nfile*> target; 
     deque<nfile*> source; 
 
-    //cout << endl << "Calling FileSearch" << endl << endl;
-    target = fileseach(&drive, targetFileName); 
+    flemgr mgr ;
 
-    //cout << endl << "Calling FileSearch" << endl << endl;
-    source = fileseach(&drive, sourceFileName); 
+    switch(mode){
+        case FILECRAWLER: 
+            mgr.buildDirectoryStructure(inputFile, &mgr.drive, "");
+            target = mgr.fileseach(targetFileName); 
+            source = mgr.fileseach(sourceFileName); 
+        break;
+        case ADDFILE: 
+            // // FolderA -> SubfolderA -> File2 -> FolderB -> SubfolderD -> File6 
+            std::cout << "Enter the path + the source file name: ";
+            std::cin >> sourceFileName;
+
+        break;
+        case RMFILE: 
+            if(mgr.drive.children.size() < 1){ 
+                cout << "nothing to delete" <<endl;
+                break;
+            }
+            std::cout << "Enter the path + source file name: ";
+            std::cin >> sourceFileName;
+            mgr.setcontentname(sourceFileName);
+ 
+            // get path 
+            //rm(filename, &drive);
+        break;
+        case ADDIR: 
+            std::cout << "Enter the path + dir name: ";
+            std::cin >> sourceFileName;
+
+        //addir(filename, &drive);
+        break; 
+        case RMDIR: 
+            if(mgr.drive.children.size() < 1){ 
+                cout << "nothing to delete" <<endl;
+                break;
+            }
+            std::cout << "Enter the path + dir name: ";
+            std::cin >> sourceFileName;
+
+            mgr.setcontentname(sourceFileName);
+
+        break;
+        case LOAD:
+
+        //if files are loaded file an error.
+        buildDirectoryStructure(inputFile, &mgr.drive);
+
+        break;
+        case SAVE:
+        break; 
+        case PURGE: 
+            mgr.fileseach(""); 
+        break;
+        case OPTIONS: 
+        cout << options << endl;
+        break; 
+        case EXIT:
+            return 0;
+        break; 
+
+        default: 
+        cout << "Option doesn't exist."<<endl;
+    };
 
     vector<string> fpath;
 
